bfb64dbd172924955fed73a01dbfbd2cd5d96967
David-Chan-Ho2 <dho12@charlotte.edu>
2026-01-05 19:25:13 -0500

    add secrets

--- app/config/database.py
+++ app/config/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-
-DATABASE_URL = "sqlite:///./app.db"
+from app.config import secrets
 
 engine = create_engine(
-    DATABASE_URL,
+    secrets.DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
